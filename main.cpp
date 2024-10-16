5487622894beb61aa5f42f23fb766f46c048c43a
shhhreya645 <shreya.pawar@kalvium.community>
2024-10-16 09:45:44 +0530

    New-and-Del

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// Class: Ingredient
+// Ingredient class
 class Ingredient {
 private:
     string name;
@@ -12,47 +12,38 @@ private:
 public:
     // Constructor
     Ingredient(string name, double quantity) {
-        // Using this pointer to resolve name conflict
         this->name = name;
         this->quantity = quantity;
     }
 
-    // Getter function to return the name of the ingredient
+    // Getter for name
     string getName() const {
         return this->name;
     }
 
-    // Set the quantity using this pointer
-    void setQuantity(double quantity) {
-        this->quantity = quantity;
-    }
-
-    // Member function to display ingredient details
+    // Display function
     void displayIngredient() const {
         cout << "Ingredient: " << this->name << ", Quantity: " << this->quantity << " units" << endl;
     }
 
-    // Function to compare quantities using this pointer
-    bool isGreaterThan(const Ingredient& other) const {
-        return this->quantity > other.quantity;
+    // Destructor
+    ~Ingredient() {
+        cout << "Ingredient " << this->name << " is being deleted." << endl;
     }
 };
 
 int main() {
-    // Create two Ingredient objects
-    Ingredient ingredient1("Sugar", 200);
-    Ingredient ingredient2("Flour", 500);
+    // Dynamically allocate memory for Ingredient objects
+    Ingredient* ingredient1 = new Ingredient("Sugar", 200);
+    Ingredient* ingredient2 = new Ingredient("Flour", 500);
 
-    // Display their details
-    ingredient1.displayIngredient();
-    ingredient2.displayIngredient();
+    // Display ingredient details
+    ingredient1->displayIngredient();
+    ingredient2->displayIngredient();
 
-    // Compare quantities using the isGreaterThan method
-    if (ingredient1.isGreaterThan(ingredient2)) {
-        cout << ingredient1.getName() << " has more quantity than " << ingredient2.getName() << endl;
-    } else {
-        cout << ingredient2.getName() << " has more quantity than " << ingredient1.getName() << endl;
-    }
+    // Clean up memory
+    delete ingredient1; // Deallocate memory for ingredient1
+    delete ingredient2; // Deallocate memory for ingredient2
 
     return 0;
 }
